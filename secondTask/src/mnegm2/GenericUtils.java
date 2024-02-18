fc6e44ba15a2d287ea9391cccd4444e1f924ada4
mnegm2 <mnegm2@student.tgm.ac.at>
2024-02-18 15:34:18 +0100

    comments

--- secondTask/src/mnegm2/GenericUtils.java
+++ secondTask/src/mnegm2/GenericUtils.java
@@ -1,6 +1,15 @@
 package mnegm2;
 
+/**
+ * Utility class for performing generic operations.
+ */
 public class GenericUtils {
+    /**
+     * Reverses the elements in the given array.
+     *
+     * @param items The array whose elements are to be reversed.
+     * @param <T>   The type of elements in the array.
+     */
     public static<T> void reverse(T[] items) {
         for (int i = 0; i < items.length/2; i++) {
             T temp =  items[i];
@@ -9,6 +18,13 @@ public class GenericUtils {
         }
     }
 
+    /**
+     * Calculates the sum of elements in the given array.
+     *
+     * @param items The array whose elements are to be summed.
+     * @param <T>   The type of elements in the array, which must extend Number.
+     * @return The sum of elements in the array.
+     */
     public static<T extends Number> double sum(T[] items) {
         double sum = 0;
         for (int i = 0; i< items.length; i++) {
--- secondTask/src/mnegm2/Main.java
+++ secondTask/src/mnegm2/Main.java
@@ -1,19 +1,26 @@
 package mnegm2;
 
+/**
+ * Main class to demonstrate the usage of GenericUtils methods.
+ */
 public class Main {
+    /**
+     * Main method to demonstrate the usage of GenericUtils methods.
+     *
+     * @param args Command line arguments (not used in this program).
+     */
     public static void main(String[] args) {
         String[] sentence = {"A", "B", "C"};
         System.out.println("first: " + sentence[0] + "; last: " + sentence[2]);
         GenericUtils.reverse(sentence);
         System.out.println("after reverse - first: " + sentence[0] + "; last: " + sentence[2]);
 
-        Double[] numbers = {1.3,2.3,3.3};
+        Double[] numbers = {1.3, 2.3, 3.3};
         System.out.println("\nfirst: " + numbers[0] + "; last: " + numbers[2]);
         GenericUtils.reverse(numbers);
         System.out.println("after reverse - first: " + numbers[0] + "; last: " + numbers[2]);
 
-
-        System.out.println("sum (ca. 6.9) - "+GenericUtils.sum(numbers));
+        System.out.println("sum (ca. 6.9) - " + GenericUtils.sum(numbers));
         //GenericUtils.sum(sentence);     --- does not work!
     }
 }
